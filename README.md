4fd0934e25dec04f2876f321fc101be092138918
Rakesh Purohit <26131905+TheRakeshPurohit@users.noreply.github.com>
2022-04-23 17:21:43 +0530

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# water-reminder
+# Water Reminder (Dev Edition)
+
 Water Reminder is an extension which will force you to drink water after evry hour you spend on your screen.
+
+![Demo Photo](./assets/banner.webp)
+
+## Tech Stack
+
+- HTML
+- CSS
+- JavaScript
---
+++ background.js
@@ -0,0 +1,5 @@
+setInterval(() => {
+    document.getElementsByTagName("body")[0].innerHTML =
+      '<div><img src="https://assets.rebelmouse.io/eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpbWFnZSI6Imh0dHBzOi8vYXNzZXRzLnJibC5tcy8yNjcxNjc0My9vcmlnaW4uanBnIiwiZXhwaXJlc19hdCI6MTY5MDczMjk3Mn0.rJn4pSX1ttIktf0APsC4SbBGjz7TkDzLmer3U92ILcQ/img.jpg" style="margin: 0px auto; height: 100vh; display: flex;"></div>';
+  }, 3600000);
+  
