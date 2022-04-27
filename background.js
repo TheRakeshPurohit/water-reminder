799ff02e2b45caa28d61ae516e6dff05564240e1
Rakesh Purohit <26131905+TheRakeshPurohit@users.noreply.github.com>
2022-04-27 16:22:52 +0530

    spell check

--- background.js
+++ background.js
@@ -1,5 +1,5 @@
 setInterval(() => {
-  if (window.confirm('Hey! I am Cristiano Ronaldo 7.I have a water reminder fro you. Should I proceed?')) {
+  if (window.confirm('Hey! I am Cristiano Ronaldo 7.I have a water reminder for you. Should I proceed?')) {
     window.open('https://raw.githubusercontent.com/TheRakeshPurohit/water-reminder/main/assets/banner.webp');
   }
 }, 3600000);
