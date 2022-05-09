74cc4df3d8b0b243d9b25493c9dd2919de0ded0d
Rakesh Purohit <26131905+TheRakeshPurohit@users.noreply.github.com>
2022-05-09 16:05:25 +0530

    time added in popup

--- background.js
+++ background.js
@@ -1,7 +1,19 @@
 setInterval(() => {
-  if (([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23].includes(new Date().getHours()) && new Date().getMinutes() === 0)) {
-    if (window.confirm('Hey! I am Cristiano Ronaldo 7.I have a water reminder for you. Should I proceed?')) {
-      window.open('https://raw.githubusercontent.com/TheRakeshPurohit/water-reminder/main/assets/banner.webp');
+  if (
+    [
+      0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+      21, 22, 23,
+    ].includes(new Date().getHours())
+    && new Date().getMinutes() === 0
+  ) {
+    if (
+      window.confirm(
+        `Hey! I am Cristiano Ronaldo 7. It's ${new Date().getHours()} o'clock. I have a water reminder for you. Should I proceed?`,
+      )
+    ) {
+      window.open(
+        'https://raw.githubusercontent.com/TheRakeshPurohit/water-reminder/main/assets/banner.webp',
+      );
     }
   }
 }, 60000);
